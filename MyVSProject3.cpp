644cad51666341b927c4e6f03d60ed794efd3703
IluKozlov <113104431+Ganstailu@users.noreply.github.com>
2022-09-27 22:04:02 +0300

    Create MyVSProject3.cpp
    lol

---
+++ MyVSProject3.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main() {
+	int x, y, z;
+	scanf_s("%d%d%d", &x, &y, &z);
+	int srednee = (x > y) ? ((z < y) ? y : ((z < x) ? z : x)) : ((x > z) ? x : (y > z) ? z : y);
+	printf("Srednee = %d\n", srednee);
+	return 0;
+}
