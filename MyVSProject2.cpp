4ee5e54f24267f9e49724de7d1dc7ae94d7bf2de
IluKozlov <113104431+Ganstailu@users.noreply.github.com>
2022-09-27 22:01:58 +0300

    Create MyVSProject2.cpp
    lol

---
+++ MyVSProject2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main() {
+	int x, y;
+	scanf_s("%d%d", &x, &y);
+	if ((x == 0) || (y == 0)) {
+		printf("Ne v kakoyi chetverti");
+	}
+	else if ((x < 0) && (y > 0)) {
+		printf("2 chetvert");
+	}
+	else if ((x < 0) && (y < 0)) {
+		printf("3 chetvert");
+	}
+	else if ((x > 0) && (y < 0)) {
+		printf("4 chetvert");
+	}
+	else if ((x > 0) && (y > 0)) {
+		printf("1 chetvert");
+	}
+	return 0;
+}
